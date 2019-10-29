85edf549a3f40c1573b985dcd6ef849b51da112b
Simon Wicki <simon.wicki@justwatch.com>
2019-10-29 23:21:32 +0100

    added: docs

--- config/site.config.js
+++ config/site.config.js
@@ -4,45 +4,43 @@ const fs = require('fs');
 let ROOT = process.env.PWD;
 
 if (!ROOT) {
-  ROOT = process.cwd();
+	ROOT = process.cwd();
 }
 
 const config = {
-  // Your website's name, used for favicon meta tags
-  site_name: 'Simon Wicki - Frontend Engineer',
+	// Your website's name, used for favicon meta tags
+	site_name: 'Simon Wicki - Frontend Engineer',
 
-  // Your website's description, used for favicon meta tags
-  site_description: 'Frontend Engineer for Ionic, VueJS, Angular, Cordova, Capacitor',
+	// Your website's description, used for favicon meta tags
+	site_description: 'Frontend Engineer for Ionic, VueJS, Angular, Cordova, Capacitor',
 
-  // Your website's URL, used for sitemap
-  site_url: 'https://wicki.io',
+	// Your website's URL, used for sitemap
+	site_url: 'https://wicki.io',
 
-  // Google Analytics tracking ID (leave blank to disable)
-  googleAnalyticsUA: '',
+	// Google Analytics tracking ID (leave blank to disable)
+	googleAnalyticsUA: '',
 
-  // The viewport meta tag added to your HTML page's <head> tag
-  viewport: 'width=device-width,initial-scale=1',
+	// The viewport meta tag added to your HTML page's <head> tag
+	viewport: 'width=device-width,initial-scale=1',
 
-  // Source file for favicon generation. 512x512px recommended.
-  favicon: path.join(ROOT, '/src/images/logo.png'),
+	// Source file for favicon generation. 512x512px recommended.
+	favicon: path.join(ROOT, '/src/images/logo.png'),
 
-  // Local development URL
-  dev_host: 'localhost',
+	// Local development URL
+	dev_host: 'localhost',
 
-  // Local development port
-  port: process.env.PORT || 8000,
+	// Local development port
+	port: process.env.PORT || 8000,
 
-  // Advanced configuration, edit with caution!
-  env: process.env.NODE_ENV,
-  root: ROOT,
-  paths: {
-    config: 'config',
-    src: 'src',
-    dist: 'dist',
-  },
-  package: JSON.parse(
-    fs.readFileSync(path.join(ROOT, '/package.json'), { encoding: 'utf-8' }),
-  ),
+	// Advanced configuration, edit with caution!
+	env: process.env.NODE_ENV,
+	root: ROOT,
+	paths: {
+		config: 'config',
+		src: 'src',
+		dist: 'docs',
+	},
+	package: JSON.parse(fs.readFileSync(path.join(ROOT, '/package.json'), { encoding: 'utf-8' })),
 };
 
 module.exports = config;
